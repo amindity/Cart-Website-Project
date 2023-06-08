b5affff15cbe3745e37a1a9a73ac92a02ebc589c
amin <amindity1@gmail.com>
2023-06-08 11:48:05 +0330

    add cart

--- src/cart/index.jsx
+++ src/cart/index.jsx
@@ -1,11 +1,42 @@
 import './cartStyle.css'
+import {AiTwotoneShop , AiFillStar} from 'react-icons/ai'
+import {BsCurrencyDollar} from 'react-icons/bs'
 
-
-const Cart = () => {
+const Cart = (props) => {
   return ( 
-    <>
-    
-    </>
+    <div className='w-100 cart--container p-4 rounded'>
+     <div className="w-100 cart--img position-relative">
+      <img className='w-100 h-100' src={props.productImg} alt="img" />
+      <div className="cart-tag position-absolute top-0 px-3 py-1">{props.productTag}</div>
+     </div>
+
+     <h5 className='py-1 m-0 text-muted text-container'>{props.productName}</h5>
+
+     <div className="p-0 text-muted">
+      <AiTwotoneShop/>
+      <span>{props.productCategory}</span>
+     </div>
+
+     <div className="d-flex align-items-center gap-1 py-1">
+      <AiFillStar className='text-warning fs-5'/>
+      <h5 className='fs-5'>{props.productRatingRate}</h5>
+      <span className='text-muted'>
+        (
+          <span>{props.productRatingCount}</span>نظر
+        )
+      </span>
+     </div>
+
+     <div className="d-flex justify-content-between py-1">
+      <div>
+        <button className='cart-button rounded'>+</button>
+      </div>
+      <div className="d-flex align-items-center">
+        <h4 className='fs-4'>{props.productPrice}</h4>
+        <BsCurrencyDollar className='fs-4'/>
+      </div>
+     </div>
+    </div>
    );
 }
  
--- src/homepage/index.jsx
+++ src/homepage/index.jsx
@@ -1,11 +1,37 @@
+import { useEffect, useState } from 'react';
 import Cart from '../cart';
 import './homepageStyle.css'
+import axios from 'axios';
 
 const Homepage = () => {
+  const [productList , setProductList] = useState([])
+
+  useEffect(()=>{
+    axios.get('https://fakestoreapi.com/products')
+    .then((data) => {setProductList(data.data)})
+    .catch((err) => console.log(err))
+  },[])
   return ( 
-    <>
-    <Cart/>
-    </>
+    <div dir='rtl'>
+      <div className="w-100 h-100 d-flex flex-wrap gap-2 justify-content-center align-items-center">
+      {productList.map(item => {
+        return(
+          <div key={`productList-${item.id}`} className="home-container">
+            <Cart 
+            productId = {item.id}
+             productImg = {item.image}
+             productName= {item.title}
+             productCategory = {item.category}
+             productRatingRate ={item.rating.rate}
+             productRatingCount = {item.rating.count}
+             productPrice = {item.price}
+             productTag = 'موجود محدود'
+            />
+          </div>
+        )
+      })}
+      </div>
+    </div>
    );
 }
  
